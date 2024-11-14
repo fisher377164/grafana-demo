b1f3fc2a135e24c0b83cfab9b282379473194032
Andrii Vdovychenko <a.vdovychenko@twinwingames.com>
2024-11-14 15:17:21 +0200

    Add demo

---
+++ src/test/java/com/fisher/game/engine/grafanademo/GrafanaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisher.game.engine.grafanademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
