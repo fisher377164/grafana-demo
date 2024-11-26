90f11d85336799cfcfdd4af76d6fc754f306a279
Andrii Vdovychenko <a.vdovychenko@twinwingames.com>
2024-11-26 16:08:10 +0200

    Add error endpoint

--- src/main/java/com/fisher/game/engine/grafanademo/web/ControllerDemo.java
+++ src/main/java/com/fisher/game/engine/grafanademo/web/ControllerDemo.java
@@ -3,7 +3,10 @@ package com.fisher.game.engine.grafanademo.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 
 @RestController
@@ -14,6 +17,14 @@ public class ControllerDemo {
     @GetMapping("/demo")
     public String demo() {
         return "Demo message";
+    }
+
+    @GetMapping("/demo/{message}")
+    public String demo(@PathVariable String message) {
+        if (Objects.equals(message, "error")) {
+            throw new RuntimeException("LOL: ERROR!");
+        }
+        return "Demo message: " + message;
     }
 
 }
